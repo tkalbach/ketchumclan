9e23cc4f122280d1095e6eb21f563987ae877d0c
Tyler Kalbach <boarder7395@hotmail.com>
2016-12-09 02:50:47 +0000

    Facebook Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
+    get 'auth/:provider/callback', to: 'sessions#create'
+    get 'auth/failure', to: redirect('/')
+    get 'signout', to: 'sessions#destroy', as: 'signout'
+
+    resources :sessions, only: [:create, :destroy]
+    resource :home, only: [:show]
+
+    root to: "home#show"
+end
+
+#Rails.application.routes.draw do
+#  get 'sessions/create'
+
+#  get 'sessions/destroy'
+
+#  get 'home/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'client#new'
+#  root :to => 'client#new'
+  
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +72,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+#end
