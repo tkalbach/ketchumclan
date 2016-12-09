b3ae4a2def2c8a3594fd0ae1007881ea8670ea51
Tyler Kalbach <boarder7395@hotmail.com>
2016-12-09 03:08:28 +0000

    Attempted Fix undefined web console'

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV[facebookid], ENV[facebookpass], {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, ENV['facebookid'], ENV['facebookpass'], {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
